680d687f225a8eb774a86aeb6e5ebfc564974c38
ChampCCDC leadership <matt@xhec.dev>
2025-03-03 11:28:11 -0500

    wheee

--- tui.py
+++ tui.py
@@ -3,18 +3,28 @@ import json
 import time
 import getpass
 import urllib3
-import csv,os
+import csv,os,sys
 from random import randint
 
 from pveautomate.automate import ProxmoxManager
 
 PROX_URL = os.getenv("PROXMOX_URL", "https://192.168.3.236:8006") + "/api2/json"
 
+def load_csv(file_name):
+    try:
+        with open(file_name) as file:
+            reader = csv.reader(file)
+            rows = [row for row in reader]
+            return rows
+    except:
+        print("Something funky")
+        return None
+
 if __name__ == "__main__":
     proxmox_url = PROX_URL
     proxmox_user = "root@pam"
     proxmox_password = getpass.getpass(f"Authenticate for {proxmox_user}: ")
-    node = "ccdc"
+    node = "pve"
 
     manager = ProxmoxManager(proxmox_url, proxmox_user, proxmox_password, node)
 
@@ -23,23 +33,39 @@ if __name__ == "__main__":
     while running:
         manager.read_vm_data()
         print(
-            """1. Create Windows range VMs for user
+            """1. Create range VMs for user
 2. Destroy single VM
 3. Destroy multiple VMs
 4. Destroy ALL range VMs
-5. Create Windows range VMs for multiple users
+5. Create range VMs for multiple users
 6. Create new user
 Q. Quit"""
         )
         c = input("> ")
         if c == "1":
-            manager.create_range([100,101])
+            vmids = input("Comma seperated list of VMIDs (or just one): ")
+            tgt = []
+            if not ',' in vmids:
+                tgt.append(int(vmids))
+            else:
+                stuff = vmids.split(",")
+                for vid in stuff:
+                    tgt.append(int(vid))
+            manager.create_range(tgt, input("Username: "))
         elif c == "5":
+            vmids = input("Comma seperated list of VMIDs (or just one): ")
+            tgt = []
+            if not ',' in vmids:
+                tgt.append(int(vmids))
+            else:
+                stuff = vmids.split(",")
+                for vid in stuff:
+                    tgt.append(int(vid))
             users = input("Comma-seperated list of users to make VMs for: ")
             for user in users.split(","):
                 if not '@' in user:
                     user = user + "@pve"
-                manager.create_range([100,101], user)
+                manager.create_range(tgt, user)
         elif c == "2":
             manager.destroy_vm(int(input("VMID to destroy (NO CONFIRMATION): ")))
         elif c == "3":
@@ -50,5 +76,22 @@ Q. Quit"""
             manager.destroy_range()
         elif c == "6":
             manager.create_user(input("Username: "), getpass.getpass("Password: "), 'pve')
+        elif c == "7":
+            fn = input("Filename CSV of users: ")
+            newpw = getpass.getpass("Default password for any users we need to create: ")
+            rows = load_csv(fn)
+            if rows is None:
+                print("No such file, or other error")
+                sys.exit(1)
+            else:
+                for index, row in enumerate(rows):
+                    username = row[0]
+                    group = "Proxmox_Users" if row[1] == "user" else "Proxmox_Admins"
+                    if not manager.check_if_user(username+"@pve"): # doesn't exist
+                        manager.create_user(username, newpw, 'pve')
+                        manager.set_user_group(username+"@pve", group)
+                        print(f"Created {username}@pve with group {group}")
+                    else:
+                        print(f"{username} exists. Might have to manually check group?")
         else:
             running = False
